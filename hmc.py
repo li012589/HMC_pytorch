ff2ccb95d13f63993c3a0f22e36417c908687172
Li SH <li012589li@outlook.com>
2019-06-26 12:46:18 +0800

    update

--- hmc.py
+++ hmc.py
@@ -1,67 +1,58 @@
 import numpy as np
 import torch
-from torch.autograd import Variable
-from utils.metropolis import metropolis
-from utils.autoCorrelation import autoCorrelationTimewithErr
-from utils.acceptRate import acceptanceRate
+from torch import nn
+from torch.autograd import grad as torchgrad
+
+def HMCwithAccept(energy,x,length,steps,epsilon):
+    shape = [i if no==0 else 1 for no,i in enumerate(x.shape)]
+    def grad(z):
+        return torchgrad(energy(z),z,grad_outputs=z.new_ones(z.shape[0]))[0]
+    torch.set_grad_enabled(False)
+    E = energy(x)
+    torch.set_grad_enabled(True)
+    g = grad(x.requires_grad_())
+    torch.set_grad_enabled(False)
+    g = g.detach()
+    for l in range(length):
+        p = x.new_empty(size=x.size()).normal_()
+        H = ((0.5*p*p).reshape(p.shape[0], -1).sum(dim=1) + E)
+        xnew = x
+        gnew = g
+        for _ in range(steps):
+            p = p- epsilon* gnew/2.
+            xnew = (xnew + epsilon * p)
+            torch.set_grad_enabled(True)
+            gnew = grad(xnew.requires_grad_())
+            torch.set_grad_enabled(False)
+            xnew = xnew.detach()
+            gnew = gnew.detach()
+            p = p- epsilon* gnew/2.
+        Enew = energy(xnew)
+        Hnew = (0.5*p*p).reshape(p.shape[0], -1).sum(dim=1) + Enew
+        diff = H-Hnew
+        accept = (diff.exp() >= diff.uniform_()).to(x)
+
+        E = accept*Enew + (1.-accept)*E
+        acceptMask = accept.reshape(shape)
+        x = acceptMask*xnew + (1.-acceptMask)*x
+        g = acceptMask*gnew + (1.-acceptMask)*g
+    torch.set_grad_enabled(True)
+
+    return x, accept
+
+def HMC(*args,**kwargs):
+    x, _ = HMCwithAccept(*args,**kwargs)
+    return x
 
 
-class HMCSampler:
-    def __init__(self,model,prior,stepSize=0.1,interSteps=10,targetAcceptRate=0.65,stepSizeMin=0.001,stepSizeMax=1000,stepSizeChangeRatio=0.03,dynamicStepSize=False):
-        pass
-        self.model = model
-        self.prior = prior
-        self.stepSize = stepSize
+class HMCsampler(nn.Module):
+    def __init__(self,energy,nvars, epsilon=0.01, interSteps=10 , thermalSteps = 10):
+        super(HMCsampler,self).__init__()
+        self.nvars = nvars
+        self.energy = energy
         self.interSteps = interSteps
-        self.dynamicStepSize = dynamicStepSize
-        if self.dynamicStepSize:
-            self.targetAcceptRate = targetAcceptRate
-            self.stepSizeMax = stepSizeMax
-            self.stepSizeMin = stepSizeMin
-            self.stepSizeInc = stepSizeChangeRatio+1
-            self.stepSizeDec = 1-stepSizeChangeRatio
+        self.inital = HMC(self.energy,torch.randn(nvars),thermalSteps,interSteps)
 
-    def updateStepSize(self,accept,stepSize):
-        ratio = torch.sum(accept)/accept.shape[0]*accept[1]
-        if ratio > self.targetAcceptRate:
-            newStepSize = stepSize*self.stepSizeInc
-        else:
-            newStepSize = stepSize*self.stepSizeDec
-        newStepSize = max(min(self.stepSizeMax,newStepSize),self.stepSizeMin)
-        return newStepSize
-
-    @staticmethod
-    def hmcUpdate(z,v,model,stepSize,interSteps):
-        force = model.backward(z)
-        vp = v - 0.5*stepSize*force
-        zp  = z + stepSize*vp
-        for i in range(interSteps):
-            force = model.backward(zp)
-            vp -= stepSize*force
-            zp += stepSize*vp
-        force = model.backward(zp)
-        vp = vp - 0.5*stepSize*force
-        return zp,vp
-
-    @staticmethod
-    def hamiltonian(energy,v):
-        return energy+0.5*torch.sum(v**2,1)
-
-    def sample(self,steps,batchSize):
-        z = self.prior(batchSize)
-        zpack = []
-        for i in range(steps):
-            v = torch.randn(z.size())
-            zp,vp = self.hmcUpdate(z,v,self.model,self.stepSize,self.interSteps)
-            accept = metropolis(self.hamiltonian(self.model(z),v),self.hamiltonian(self.model(zp),vp))
-            if self.dynamicStepSize:
-                self.stepSize = self.updateStepSize(accept,self.stepSize)
-            #print(type(accept.numpy()))
-            accept = np.array([accept.numpy()]*self.model.size).transpose()
-            mask = 1-accept
-            z = torch.from_numpy(z.numpy()*mask +zp.numpy()*accept)
-            zpack.append(z.numpy())
-        return zpack
-
-if __name__ == "__main__":
-    pass
+    def step(self):
+        self.inital = HMC(self.energy,self.inital,1,interSteps,epsilon)
+        return self.inital
